0311e3aec007e33a4c610435bb10fd356b45c096
Vanessa Monteiro <contato.vanessalima7@gmail.com>
2023-05-05 17:06:24 -0300

    commit desafio 01

---
+++ desafio01/index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>Desafio 01: Antecessor e Sucessor</title>
+
+    <link rel="stylesheet" href="style.css">
+
+</head>
+
+<body>
+
+    <header>
+        <h1>Antecessor e Sucessor</h1>
+    </header>
+
+    <section>
+
+        <form action="cad.php" method="get">
+
+            <label for="numero">Digite um número </label>
+            <input type="number" name="numero" id="idnumero">
+
+            <input type="submit" value="Enviar">
+
+        </form>
+
+    </section>
+
+</body>
+
+</html>
